2104bc120f18ad9987a0b7a991c679812ad0bc57
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-05-29 17:31:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import store from './data/store.js';
+    import axios from 'axios'
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/data/store.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue';
+
+const iMieiDati = reactive({
+    searchInput: ""
+});
+
+export default iMieiDati;
